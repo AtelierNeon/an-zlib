64fc0be37f4d390ecfaaab58e8907c1e0a77a0c0
Kene Lin <kenelin@gmail.com>
2023-09-05 07:33:33 +0800

    Update VCRT policy

--- source/cmake-modules/MSVC-MyRuntimeConfig.cmake
+++ source/cmake-modules/MSVC-MyRuntimeConfig.cmake
@@ -15,41 +15,49 @@ endif ()
 if (BUILD_WITH_SHARED_VCRT)
   ## Presets:
   ##   Use /MD build instead of /MT to be dependent on msvcrtXXX.dll
-  foreach (flag_var
-      CMAKE_C_FLAGS
-      CMAKE_C_FLAGS_DEBUG
-      CMAKE_C_FLAGS_RELEASE
-      CMAKE_C_FLAGS_MINSIZEREL
-      CMAKE_C_FLAGS_RELWITHDEBINFO
-      CMAKE_CXX_FLAGS
-      CMAKE_CXX_FLAGS_DEBUG
-      CMAKE_CXX_FLAGS_RELEASE
-      CMAKE_CXX_FLAGS_MINSIZEREL
-      CMAKE_CXX_FLAGS_RELWITHDEBINFO)
-    if (${flag_var} MATCHES "/MT")
-      string (REGEX REPLACE "/MT" "/MD" ${flag_var} "${${flag_var}}")
-    endif ()
-  endforeach ()
+  if (NOT CMAKE_VERSION VERSION_LESS 3.15)
+    set (CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>DLL")
+  else ()
+    foreach (flag_var
+        CMAKE_C_FLAGS
+        CMAKE_C_FLAGS_DEBUG
+        CMAKE_C_FLAGS_RELEASE
+        CMAKE_C_FLAGS_MINSIZEREL
+        CMAKE_C_FLAGS_RELWITHDEBINFO
+        CMAKE_CXX_FLAGS
+        CMAKE_CXX_FLAGS_DEBUG
+        CMAKE_CXX_FLAGS_RELEASE
+        CMAKE_CXX_FLAGS_MINSIZEREL
+        CMAKE_CXX_FLAGS_RELWITHDEBINFO)
+      if (${flag_var} MATCHES "/MT")
+        string (REGEX REPLACE "/MT" "/MD" ${flag_var} "${${flag_var}}")
+      endif ()
+    endforeach ()
+  endif ()
 endif ()
 
 if (BUILD_WITH_STATIC_VCRT)
   ## Presets:
   ##   Use /MT build instead of /MD to avoid msvcrtXXX.dll
-  foreach (flag_var
-      CMAKE_C_FLAGS
-      CMAKE_C_FLAGS_DEBUG
-      CMAKE_C_FLAGS_RELEASE
-      CMAKE_C_FLAGS_MINSIZEREL
-      CMAKE_C_FLAGS_RELWITHDEBINFO
-      CMAKE_CXX_FLAGS
-      CMAKE_CXX_FLAGS_DEBUG
-      CMAKE_CXX_FLAGS_RELEASE
-      CMAKE_CXX_FLAGS_MINSIZEREL
-      CMAKE_CXX_FLAGS_RELWITHDEBINFO)
-    if (${flag_var} MATCHES "/MD")
-      string (REGEX REPLACE "/MD" "/MT" ${flag_var} "${${flag_var}}")
-    endif ()
-  endforeach ()
+  if (NOT CMAKE_VERSION VERSION_LESS 3.15)
+    set (CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>")
+  else ()
+    foreach (flag_var
+        CMAKE_C_FLAGS
+        CMAKE_C_FLAGS_DEBUG
+        CMAKE_C_FLAGS_RELEASE
+        CMAKE_C_FLAGS_MINSIZEREL
+        CMAKE_C_FLAGS_RELWITHDEBINFO
+        CMAKE_CXX_FLAGS
+        CMAKE_CXX_FLAGS_DEBUG
+        CMAKE_CXX_FLAGS_RELEASE
+        CMAKE_CXX_FLAGS_MINSIZEREL
+        CMAKE_CXX_FLAGS_RELWITHDEBINFO)
+      if (${flag_var} MATCHES "/MD")
+        string (REGEX REPLACE "/MD" "/MT" ${flag_var} "${${flag_var}}")
+      endif ()
+    endforeach ()
+  endif ()
 endif ()
 
 if (BUILD_WITH_WORKAROUND_OPT_GY)
